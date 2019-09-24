ae403695cb12b6447a94950e6e282fec9f81006c
Giovanni <g.l.mierop@gmail.com>
2019-09-24 11:08:22 +0200

    Start of gaem

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+
+
+function startGame() {
+    alert("*you wake up in an infamiliar place*\n"
+        + "Your surroundings are shaking, it looks like you're in a van.\n"
+        + "You can't really remember how you got here, everyhing is a little fuzzy.");
+
+    alert("Hey, you. You're finally awake.\n"
+        + "You were trying to raid Area 51, right?\n"
+        + "Walked right into that Guard ambush, same as us, and that Naruto Runner over there.\n"
+        + "Damn you Guards! We just want to see them Aliens.");
+
+    var name = prompt("So what's your name?", "Type your name here.");
+    var ans = prompt("What gender do you identify as?\n\n1: Boi\n2: Whamen\n\nType 1 or 2 or something else idc");
+    var gender;
+    if (ans == 1) {
+        gender = "boi";
+    }
+    if (ans == 2) {
+        gender = "whamen";
+    }
+    else {
+        gender = ans;
+    }
+    "Alright " + name + " "
+
+
+}
